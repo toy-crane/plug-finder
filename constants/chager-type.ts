8742b7d5217eda34517c130c9a185201b0a6ff57
toycrane <alwaysfun2183@gmail.com>
2024-01-10 21:34:24 +0900

    feat(charger-type): add charger type constants and function

---
+++ constants/chager-type.ts
@@ -0,0 +1,22 @@
+export const chargerType = {
+  1: "DC 차데모",
+  2: "AC 완속",
+  3: "DC차데모, AC3상",
+  4: "DC 콤보",
+  5: "DC차데모, DC콤보",
+  6: "DC차데모, AC3상, DC콤보",
+  7: "AC3상",
+  8: "DC 콤보(완속)",
+  89: "H2",
+} as const;
+
+type ChargerType = typeof chargerType;
+type ChargerCode = keyof ChargerType;
+
+function getChargerTypeDescription(code: string): string | undefined {
+  const parsedCode = parseInt(code, 10);
+  if (!isNaN(parsedCode) && chargerType.hasOwnProperty(parsedCode)) {
+    return chargerType[parsedCode as ChargerCode];
+  }
+  throw new Error("Invalid region code");
+}
