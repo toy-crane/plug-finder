90093f64bb5315bb63e6a536a18f0d5d4a17531d
toycrane <alwaysfun2183@gmail.com>
2024-01-10 14:30:13 +0900

    style: Add CSS grid to app layout

--- app/stations/layout.tsx
+++ app/stations/layout.tsx
@@ -1,5 +1,5 @@
 const Layout = (props: { children: React.ReactNode }) => {
-  return <div>{props.children}</div>;
+  return <div className="content-grid">{props.children}</div>;
 };
 
 export default Layout;
