ae7aa73a746c353ab1d94fad8dea1d109aa9d199
toycrane <alwaysfun2183@gmail.com>
2024-01-10 10:04:08 +0900

    refactor: Remove unused code and dependencies

--- app/stations/[slug]/page.tsx
+++ app/stations/[slug]/page.tsx
@@ -5,24 +5,6 @@ interface Props {
   params: { slug: string };
 }
 
-// Set your API key here
-const serviceKey =
-  "KZ2gsvilf6x8JPRU61b%2Frh%2BzkLv5YiCpEF%2Fm%2F0bTRJttrEnFaps5QmeWFQi9qb%2BIIKtLGpCmC57B%2Fw4Jpio9PA%3D%3D";
-
-// API endpoint for getting charger information
-const url = "http://apis.data.go.kr/B552584/EvCharger/getChargerInfo";
-const params = {
-  numOfRows: "9999",
-  pageNo: "1",
-  zscode: "46110",
-};
-
-// Create a URLSearchParams object from the params object
-const searchParams = new URLSearchParams(params);
-
-// Append serviceKey without encoding
-const queryString = `${searchParams.toString()}&serviceKey=${serviceKey}&dataType=JSON`;
-
 export async function generateStaticParams() {
   const supabase = createSupabaseBrowerClient();
   const response = await supabase.from("stations").select("slug");
