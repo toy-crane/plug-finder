a7830c86aa770e3206085569a7698eb5f23b5879
toycrane <alwaysfun2183@gmail.com>
2024-01-10 22:05:28 +0900

    refactor: Improve logo component code

--- components/nav/logo.tsx
+++ components/nav/logo.tsx
@@ -1,19 +1,19 @@
-"use client";
-
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const Logo = () => {
-  const router = useRouter();
   return (
-    <Image
-      src="/logo-filled.png"
-      width={48}
-      height={48}
-      alt="Logo"
-      className="align-middle cursor-pointer"
-      onClick={() => router.push("/")}
-    />
+    <>
+      <Link href="/">
+        <Image
+          src="/logo-filled.png"
+          width={48}
+          height={48}
+          alt="Logo"
+          className="align-middle cursor-pointer"
+        />
+      </Link>
+    </>
   );
 };
 
