9049d23ab595208410cbac7f4a84d4d86b2f4516
toycrane <alwaysfun2183@gmail.com>
2024-01-10 20:48:56 +0900

    feat(config): add site configuration for Plug Finder app

---
+++ config/site.ts
@@ -0,0 +1,16 @@
+export const siteConfig = {
+  name: "플러그 파인더",
+  description:
+    "플러그 파인더와 함께 전국의 전기차 충전소를 쉽고 빠르게 찾아보세요. 대한민국 전역의 충전소 위치, 이용 가능 시간, 서비스 상세 정보를 한눈에 확인 하실 수 있습니다. 여러분의 전기차 여정을 더욱 편리하고, 빠르게 만들어 드리겠습니다.",
+  url: "https://www.plugfinder.app",
+  ogImage: "https://www.plugfinder.app/api/og",
+  author: "toy-crane",
+  links: {
+    twitter: "https://twitter.com/alwaysfun2183",
+    github: "https://github.com/toy-crane",
+    blog: "https://www.toycrane.xyz",
+    survey: "https://forms.gle/mytCMe9N6giYZzKK7",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
