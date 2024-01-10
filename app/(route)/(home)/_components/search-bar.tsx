b18373636231dbfbb00733bf71f731a8eb28dc98
toycrane <alwaysfun2183@gmail.com>
2024-01-10 15:16:42 +0900

    feat(search-bar): add search bar component

---
+++ app/(route)/(home)/_components/search-bar.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+const SearchBar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Button
+        variant="outline"
+        className={cn(
+          "relative h-12 w-full justify-start rounded-[0.5rem] bg-background text-md font-normal text-muted-foreground shadow-none sm:pr-12 w-full"
+        )}
+        onClick={() => setOpen(true)}
+      >
+        <span className="hidden lg:inline-flex">
+          주소 또는 충전소 이름을 입력해주세요.
+        </span>
+        <span className="inline-flex lg:hidden">충전소 또는 주소 입력</span>
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder=" 주소 또는 충전소 이름을 입력해주세요." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="충전소 이름">
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+};
+
+export default SearchBar;
--- app/(route)/(home)/layout.tsx
+++ app/(route)/(home)/layout.tsx
@@ -1,8 +1,12 @@
+import SearchBar from "./_components/search-bar";
+
 const Layout = (props: { children: React.ReactNode }) => {
   return (
     <>
       <header className="content-grid">
-        <nav>Hello</nav>
+        <nav className="py-2">
+          <SearchBar />
+        </nav>
       </header>
       <main className="content-grid min-h-screen">{props.children}</main>
     </>
