aa267f35382958b0117df921d9261ff887e3ca60
toycrane <alwaysfun2183@gmail.com>
2024-01-10 18:08:42 +0900

    feat(home): Add logo component to layout

---
+++ app/(route)/(home)/_components/logo.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <Image
+      src="/logo-filled.png"
+      width={48}
+      height={48}
+      alt="Logo"
+      className="align-middle"
+      onClick={() => router.push("/")}
+    />
+  );
+};
+
+export default Logo;
--- app/(route)/(home)/layout.tsx
+++ app/(route)/(home)/layout.tsx
@@ -1,10 +1,13 @@
+import Logo from "./_components/logo";
 import SearchBar from "./_components/search-bar";
+import Image from "next/image";
 
 const Layout = (props: { children: React.ReactNode }) => {
   return (
     <>
       <header className="content-grid">
-        <nav className="py-2">
+        <nav className="py-2 flex items-center">
+          <Logo />
           <SearchBar />
         </nav>
       </header>
