a731e98ea3a3e14b749671c3b56f2e1a6e1c1a19
toycrane <alwaysfun2183@gmail.com>
2024-01-11 15:03:30 +0900

    feat: Update ogImage URL

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
   description:
     "플러그 파인더와 함께 전국의 전기차 충전소를 쉽고 빠르게 찾아보세요. 대한민국 전역의 충전소 위치, 이용 가능 시간, 서비스 상세 정보를 한눈에 확인 하실 수 있습니다. 여러분의 전기차 여정을 더욱 편리하고, 빠르게 만들어 드리겠습니다.",
   url: "https://www.plugfinder.app",
-  ogImage: "https://www.plugfinder.app/api/og",
+  ogImage: "https://www.plugfinder.app/og-image.png",
   author: "toy-crane",
   links: {
     twitter: "https://twitter.com/alwaysfun2183",
