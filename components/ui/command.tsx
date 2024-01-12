dd3208ff0c1843d42859540d0b7d26fcdfb21b23
toycrane <alwaysfun2183@gmail.com>
2024-01-12 23:04:10 +0900

    style: Update command dialog layout for mobile devices

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -34,7 +34,8 @@ const CommandDialog = ({
 }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0 shadow-lg top-0 translate-y-0 mt-4">
+      {/* 모바일 일 때, 화면 상단에서 검색창 나오도록 변경 */}
+      <DialogContent className="overflow-hidden p-0 shadow-lg top-0 translate-y-0 mt-4 md:top-[50%] md:translate-y-[-50%]">
         <Command
           className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5"
           shouldFilter={shouldFilter}
