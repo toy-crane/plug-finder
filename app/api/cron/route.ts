54946fc490d608988415086889b90cce04b3e307
toycrane <alwaysfun2183@gmail.com>
2024-01-13 23:00:33 +0900

    chore(route.ts): update Supabase service role key

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -76,7 +76,7 @@ type AddDiplayNameStation = AddChargerStation & {
 
 const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 // Set your API key here
