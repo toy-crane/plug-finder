26db9339adf27fef5fd0f5e3416cb8f364f42ffa
toycrane <alwaysfun2183@gmail.com>
2024-01-13 22:18:52 +0900

    feat: add error component for handling 500 errors

---
+++ app/(route)/stations/[z_code]/[zs_code]/[slug]/error.tsx
@@ -0,0 +1,27 @@
+"use client"; // Error components must be Client Components
+
+import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error("what is wrong?", error);
+  }, [error]);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <h1 className="text-4xl font-bold text-gray-800">500 Error</h1>
+      <p className="text-lg text-gray-600 mt-4 mb-8">
+        예상치 못한 에러가 발생했습니다. 잠시 후 다시 시도해 주세요.
+      </p>
+      <Button onClick={() => reset()}>다시 시도하기</Button>
+    </div>
+  );
+}
