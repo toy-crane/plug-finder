9ccabcb61efd1d29f9036c83f40cf0b806c1b821
toycrane <alwaysfun2183@gmail.com>
2024-01-15 11:09:47 +0900

    feat: Add middleware for logging request URL

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+
+  // x-pathname 헤더를 추가하여 요청 URL을 기록
+  requestHeaders.set(
+    "x-pathname",
+    request.nextUrl.pathname + request.nextUrl.search
+  );
+
+  console.log("middleware called");
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+  return response;
+}
