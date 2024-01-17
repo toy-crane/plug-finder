108a60654a60ad606cd4081708348427ac12cfa4
toycrane <alwaysfun2183@gmail.com>
2024-01-17 15:16:40 +0900

    fix(map): update minLevel for MarkerClusterer

--- components/map/index.tsx
+++ components/map/index.tsx
@@ -53,7 +53,7 @@ const Map = ({ markers, center, level, className }: Props) => {
     >
       <MarkerClusterer
         averageCenter={true} // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-        minLevel={7} // 클러스터 할 최소 지도 레벨
+        minLevel={5} // 클러스터 할 최소 지도 레벨
         styles={[
           {
             // calculator 각 사이 값 마다 적용될 스타일을 지정한다
