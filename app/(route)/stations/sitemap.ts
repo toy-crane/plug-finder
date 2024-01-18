6354248202599d6937fbbfa9d0292fa65af1f664
toycrane <alwaysfun2183@gmail.com>
2024-01-18 20:34:56 +0900

    chore: add more items to sitemap

--- app/(route)/stations/sitemap.ts
+++ app/(route)/stations/sitemap.ts
@@ -8,7 +8,7 @@ const addPathToBaseURL = (path: string) => `${siteConfig.url}${path}`;
 
 export async function generateSitemaps() {
   // Fetch the total number of products and calculate the number of sitemaps needed
-  return [{ id: 0 }, { id: 1 }];
+  return [{ id: 0 }, { id: 1 }, { id: 2 }];
 }
 
 export default async function sitemap({
