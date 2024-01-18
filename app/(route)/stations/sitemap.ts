f9b19f46369fc701dfd7a7c95c049cf580898532
toycrane <alwaysfun2183@gmail.com>
2024-01-18 19:28:49 +0900

    feat: Add sitemap generation and routing

---
+++ app/(route)/stations/sitemap.ts
@@ -0,0 +1,60 @@
+import { siteConfig } from "@/config/site";
+import { districtCodes } from "@/constants/districts";
+import { regionCodes } from "@/constants/regions";
+import { createSupabaseServerClientReadOnly } from "@/supabase/server";
+import { MetadataRoute } from "next";
+
+const addPathToBaseURL = (path: string) => `${siteConfig.url}${path}`;
+
+export async function generateSitemaps() {
+  // Fetch the total number of products and calculate the number of sitemaps needed
+  return [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }];
+}
+
+export default async function sitemap({
+  id,
+}: {
+  id: number;
+}): Promise<MetadataRoute.Sitemap> {
+  // Google's limit is 50,000 URLs per sitemap
+  const start = id * 40000;
+  const end = start + 40000;
+  const supabase = await createSupabaseServerClientReadOnly();
+  const response = await supabase
+    .from("stations")
+    .select("slug, z_code, zs_code")
+    .range(start, end);
+  if (response.error) {
+    throw Error("stations fetch에 실패하였습니다.");
+  }
+
+  const allStations = response.data;
+
+  const stationDetailUrls = allStations.map((st) => ({
+    url: addPathToBaseURL(
+      `/stations/${st.z_code}/${st.zs_code}/${encodeURIComponent(st.slug)}`
+    ),
+    lastModified: new Date(),
+    priority: 0.8,
+    changeFrequency: "daily" as const,
+  }));
+
+  if (id === 0) {
+    const regionUrls = regionCodes.map((code) => ({
+      url: addPathToBaseURL(`/stations/${code}`),
+      lastModified: new Date("2024-01-10"),
+      priority: 0.3,
+      changeFrequency: "monthly" as const,
+    }));
+
+    const districtUrls = districtCodes.map((code) => ({
+      url: addPathToBaseURL(`/stations/${code.substring(0, 2)}/${code}`),
+      lastModified: new Date("2024-01-10"),
+      priority: 0.5,
+      changeFrequency: "monthly" as const,
+    }));
+    return [...regionUrls, ...districtUrls, ...stationDetailUrls];
+  }
+
+  return stationDetailUrls;
+}
---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,34 @@
+import { siteConfig } from "@/config/site";
+
+const addPathToBaseURL = (path: string) => `${siteConfig.url}${path}`;
+
+export async function GET() {
+  const sitemaps = [
+    { url: "/stations/sitemap.xml/0" },
+    { url: "/stations/sitemap.xml/1" },
+  ];
+
+  const xmlStr = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://www.plugfinder.app</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1</priority>
+      </url>
+      ${sitemaps
+        .map(
+          (sm) => `<sitemap>
+          <loc>${addPathToBaseURL(sm.url)}</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+          </sitemap>`
+        )
+        .join("")}
+    </urlset>`;
+
+  return new Response(xmlStr, {
+    headers: {
+      "content-type": "application/xml;charset=UTF-8",
+    },
+  });
+}
