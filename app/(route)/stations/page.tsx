9abdc56a8525dfac65700e14c51baa4a7bbc37f6
toycrane <alwaysfun2183@gmail.com>
2024-01-18 17:25:02 +0900

    feat: add generateMetadata function

--- app/(route)/stations/page.tsx
+++ app/(route)/stations/page.tsx
@@ -3,6 +3,49 @@ import { Regions, getRegionDescription } from "@/constants/regions";
 import Link from "next/link";
 import ShareButton from "./[z_code]/[zs_code]/_component/share-button";
 import { createSupabaseServerClientReadOnly } from "@/supabase/server";
+import { Metadata, ResolvingMetadata } from "next";
+
+export async function generateMetadata(
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const supabase = await createSupabaseServerClientReadOnly();
+  const response = await supabase
+    .from("stations")
+    .select("*", { count: "exact" });
+  if (response.error) throw response.error;
+  const count = response.count;
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images ?? [];
+
+  if (count) {
+    const title = `대한민국의 모든 전기차 주차장 충전소 리스트`;
+    const description = `
+      대한민국에는 전기차 충전소가 ${count.toLocaleString()}개의 충전소(급속, 완속)가 있습니다.
+      보다 자세한 정보가 궁금하다면? 플러그 파인더 홈페이지에서 확인하세요.
+      주차 가능 여부, 외부인 개방, 영업시간, 충전기 타입, 충전기 출력 등의 모든 정보를 확인 하실 수 있습니다.
+    `;
+    return {
+      title,
+      description,
+      openGraph: {
+        title,
+        description,
+        images: [...previousImages],
+      },
+      twitter: {
+        title,
+        description,
+        images: [...previousImages],
+      },
+      alternates: {
+        canonical: `/stations`,
+      },
+    };
+  } else {
+    return {};
+  }
+}
 
 const Page = async () => {
   const regionCodes = Object.keys(Regions).filter(
