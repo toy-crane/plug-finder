32f2aeb7f403cac070322228d412d60ffeb01422
toycrane <alwaysfun2183@gmail.com>
2024-01-19 15:49:29 +0900

    style: Add z-index variables and fix semicolon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -58,6 +58,11 @@ const config = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      zIndex: {
+        header: "var(--header-z-index)",
+        "bottom-nav": "var(--bottom-nav-z-index)",
+        sheet: "var(--sheet-z-index)",
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -75,6 +80,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
