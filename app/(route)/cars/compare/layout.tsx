0d8e95565389e9192e2813bd33b8d907b18d75ea
toycrane <alwaysfun2183@gmail.com>
2024-02-01 16:42:43 +0900

    feat: add text-logo

--- app/(route)/cars/compare/layout.tsx
+++ app/(route)/cars/compare/layout.tsx
@@ -1,11 +1,15 @@
+import { Icons } from "@/components/icons";
 import Logo from "@/components/nav/logo";
+import Link from "next/link";
 
 const Layout = (props: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen bg-[#fafafa]">
       <header className="content-grid bg-white">
-        <nav className="py-3 flex items-center">
-          <Logo className="mr-2" />
+        <nav className="py-4 flex items-center">
+          <Link href="/">
+            <Icons.textLogo className="w-32 h-8" />
+          </Link>
         </nav>
       </header>
       <main className="content-grid flex-grow pb-36">{props.children}</main>
