1a62d10ab5a2224efe7287a32b3d19c34a0398c1
toycrane <alwaysfun2183@gmail.com>
2024-02-01 20:39:23 +0900

    style: Add padding to AccordionContent components

--- app/(route)/cars/compare/_components/spec.tsx
+++ app/(route)/cars/compare/_components/spec.tsx
@@ -22,31 +22,31 @@ const Spec = ({
     <Accordion type="single" collapsible defaultValue="summary">
       <AccordionItem value="summary">
         <AccordionTrigger>요약</AccordionTrigger>
-        <AccordionContent>
+        <AccordionContent className="py-4 pb-8">
           <Summary primaryId={primaryCarId} secondaryId={secondaryCarId} />
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="performance">
         <AccordionTrigger>주행 성능</AccordionTrigger>
-        <AccordionContent>
+        <AccordionContent className="py-4">
           <Performance primaryId={primaryCarId} secondaryId={secondaryCarId} />
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="charging">
         <AccordionTrigger>충전</AccordionTrigger>
-        <AccordionContent>
+        <AccordionContent className="py-4">
           <Battery primaryId={primaryCarId} secondaryId={secondaryCarId} />
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="dimension">
         <AccordionTrigger>차량 크기</AccordionTrigger>
-        <AccordionContent>
+        <AccordionContent className="py-4">
           <Dimension primaryId={primaryCarId} secondaryId={secondaryCarId} />
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="warranty">
         <AccordionTrigger>차량 보증</AccordionTrigger>
-        <AccordionContent>
+        <AccordionContent className="py-4">
           <Warranty primaryId={primaryCarId} secondaryId={secondaryCarId} />
         </AccordionContent>
       </AccordionItem>
